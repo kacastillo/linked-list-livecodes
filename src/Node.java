2863dbb51b572ae6c368aa835c444f35c74b3630
kelley <kelleycastillo@outlook.com>
2026-04-28 11:58:42 -0700

    followed along in class

--- src/Node.java
+++ src/Node.java
@@ -1,3 +1,10 @@
 public class Node {
-    
+   public char value; //instance variable
+   public Node next;
+
+   public Node(char value, Node next) {
+    this.value = value;
+    this.next = next;
+
+   }
 }
--- src/Practice.java
+++ src/Practice.java
@@ -1,5 +1,23 @@
 public class Practice {
     public static void main(String[] args) {
-        System.out.println("~linked lists practice~");
+    Node myNode = new Node('w', null);
+    Node otherNode = new Node('x', null);
+    Node coolNode = new Node('e', null);
+
+    Node current = myNode;
+    myNode.next = otherNode;
+    otherNode.next = coolNode;
+
+    while(current != null) {
+        System.out.println(current.value);
+        current = current.next;
+    }
+
+
+
+    // System.out.println(myNode.value);
+    // System.out.println(otherNode.value);
+    // System.out.println(coolNode.value);
+
     }
 }
