18fbfa25919548d2433ed814ae76790a3826650a
kelley <kelleycastillo@outlook.com>
2026-05-14 14:39:39 -0700

    follow inc lass

--- src/Practice.java
+++ src/Practice.java
@@ -1,23 +1,70 @@
 public class Practice {
     public static void main(String[] args) {
-    Node myNode = new Node('w', null);
-    Node otherNode = new Node('x', null);
-    Node coolNode = new Node('e', null);
+        Node myNode = new Node('w', null);
 
-    Node current = myNode;
-    myNode.next = otherNode;
-    otherNode.next = coolNode;
+        Node otherNode = new Node('x', null);
 
-    while(current != null) {
-        System.out.println(current.value);
-        current = current.next;
+        Node coolNode = new Node('e', null);
+        Node helloNode = new Node('f', null);
+
+        myNode.next = otherNode;
+        otherNode.next = coolNode;
+        coolNode.next = helloNode;
+        
+        myNode.next = otherNode;
+        otherNode.next = coolNode;
+
+        Node newHead = removeAt(myNode, 2);
+        Node current = newHead;
+
+        while (current != null){
+            System.out.println(current.value);
+            current = current.next;
+        }
+
+        // int numNodes = length(myNode);
+        // System.out.println(numNodes);
+
+    //     Node current = myNode;
+    //     while (current != null){
+    //         System.out.println(current.value);
+    //         current = current.next;
+    // }
+
+
+        // System.out.println(myNode.value);
+        // System.out.println(otherNode.value);
+        // System.out.println(coolNode.value);
     }
 
+    // count how many nodes are in the linked list
+    public static int length (Node head) {
+        int count  = 0;
+        Node current = head;
+        while (current != null) {
+            count++;
+            current = current.next;
+        }
 
+        return count;
+    }
+    public static Node removeAt( Node head, int removeIdx){
+        if (removeIdx < 0){
+            throw new IndexOutOfBoundsException("removeIndx must be >= 0");
+        }
+        if (removeIdx ==0){
+            return head.next;
+        }
+        Node current = head;
+        
 
-    // System.out.println(myNode.value);
-    // System.out.println(otherNode.value);
-    // System.out.println(coolNode.value);
-
+        for (int i = 0; i < removeIdx - 1;i++) {
+            if (current == null){
+                throw new IndexOutOfBoundsException("removeIdx must be < len(list");
+            }
+            current = current.next;
+        }
+        current.next= current.next.next;
+        return head;
     }
 }
